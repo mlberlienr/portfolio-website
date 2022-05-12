e2a4db2624c83fbd40dd9879857a39f834180fa0
mlberlienr <mlberliner1@gmail.com>
2022-05-11 22:01:28 -0400

    Javascript CSS and additional pages added
    changed CSS style colors and button designs
    
    Added javascript to the navigation bar to have drop shadow on scroll and added drop shadow to drop down menu.
    
    Pages added for each project.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,18 @@ function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
+    navBar.classList.add('scrolled');
   } else {
     x.className = 'navtoggle';
+    navBar.classList.remove('scrolled');
+  }
+}
+
+var navBar = document.getElementById('navBar');
+window.onscroll = function() {
+  if(window.scrollY > 22){
+    navBar.classList.add('scrolled');
+  }else{
+    navBar.classList.remove('scrolled');
   }
 }
